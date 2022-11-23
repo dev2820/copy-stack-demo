3fdcb36294f5d281fbda0326d310263b17e3b2b9
dev2820 <second28200@gmail.com>
2022-11-24 00:01:13 +0900

    refactor/ add response constants

--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,15 @@
 const contentList = [];
-let counter = 0;
+const RESPONSE = {
+  NEW_CONTENT: "NEW_CONTENT",
+  GET_CONTENT_LIST: "GET_CONTENT_LIST",
+};
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   switch (message.action) {
-    case "NEW_CONTENT": {
-      contentList.push(message.content);
-
-      await chrome.runtime.sendMessage({
-        action: "APPEND",
-        content: message.content,
-      });
+    case RESPONSE.NEW_CONTENT: {
+      contentList.push(message.payload);
     }
-    case "GET_CONTENT_LIST": {
+    case RESPONSE.GET_CONTENT_LIST: {
       sendResponse(contentList);
-    }
-    case "ADD": {
-      counter++;
-    }
-    case "GET_COUNTER": {
-      sendResponse(counter);
     }
   }
 });
