77a420907409ee3a4269d711101d8c5b5117f57b
dev2820 <second28200@gmail.com>
2022-11-23 23:36:28 +0900

    feature/ create Request class (factory design pattern)

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,21 @@
+"use strict";
+
 document.addEventListener("copy", async (evt) => {
   const text = await navigator.clipboard.readText();
 
-  await chrome.runtime.sendMessage({
-    action: "NEW_CONTENT",
-    content: text,
-  });
+  await chrome.runtime.sendMessage(
+    Request.create(Request.ACTIONS.NEW_CONTENT, text)
+  );
 });
+
+class Request {
+  static ACTIONS = {
+    NEW_CONTENT: "NEW_CONTENT",
+  };
+  static create(action, content) {
+    return {
+      action,
+      content,
+    };
+  }
+}
