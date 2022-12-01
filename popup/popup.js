4ac519d0c318b26a523177ff2c0bb80170611f43
dev2820 <second28200@gmail.com>
2022-12-02 01:03:55 +0900

    feature/ render view when item delete

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,27 @@
 window.onload = async () => {
-  const $itemList = document.getElementById("item-list");
+  const contentList = await getContentList();
+  render(contentList);
+};
+window.addEventListener("deleteitem", async (evt) => {
+  const isSuccess = await chrome.runtime.sendMessage(
+    Request.create(Request.ACTIONS.DELETE, evt.detail.id)
+  );
+
+  if (isSuccess) {
+    const contentList = await getContentList();
+    render(contentList);
+  }
+});
+
+async function getContentList() {
   const contentList = await chrome.runtime.sendMessage(
     Request.create(Request.ACTIONS.GET_CONTENT_LIST)
   );
-
+  return contentList;
+}
+function render(contentList) {
+  const $itemList = document.getElementById("item-list");
+  $itemList.innerHTML = "";
   const $list = contentList.reduce(($fragment, item) => {
     const $copiedItem = new CopiedItemElement(item);
     $fragment.appendChild($copiedItem);
@@ -12,11 +30,11 @@ window.onload = async () => {
   }, document.createDocumentFragment());
 
   $itemList.appendChild($list);
-};
-
+}
 class Request {
   static ACTIONS = {
     GET_CONTENT_LIST: "GET_CONTENT_LIST",
+    DELETE: "DELETE",
   };
   static create(action, payload) {
     return {
