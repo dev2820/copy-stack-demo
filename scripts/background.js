207b24e3f87e7bc9dbc1907cddc6a67779cf9b15
dev2820 <second28200@gmail.com>
2022-12-08 17:59:21 +0900

    feature/ create My Context Menu

--- scripts/background.js
+++ scripts/background.js
@@ -28,6 +28,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+chrome.contextMenus.create({
+  title: "My Context Menu",
+  id: "MyContext",
+});
 /**
  * onMessage에서 비동기를 쓰려면 return true를 해줘야한다함
  * https://stackoverflow.com/questions/48107746/chrome-extension-message-not-sending-response-undefined
