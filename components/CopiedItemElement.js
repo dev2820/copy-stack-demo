89cff4f3c33e0923a027851b5ca174efecbe1007
dev2820 <second28200@gmail.com>
2022-12-09 02:54:52 +0900

    feature/ image copy cover images not png (jpeg etc...)

--- components/CopiedItemElement.js
+++ components/CopiedItemElement.js
@@ -17,8 +17,8 @@ class CopiedItemElement extends HTMLElement {
     const $copyButton = $container.querySelector(".copy");
     const $deleteButton = $container.querySelector(".delete");
 
-    $copyButton.addEventListener("click", () => {
-      toClipboard(data.content);
+    $copyButton.addEventListener("click", async () => {
+      await toClipboard(data.content);
     });
     $deleteButton.addEventListener("click", () => {
       window.dispatchEvent(
@@ -38,14 +38,41 @@ class CopiedItemElement extends HTMLElement {
 
 customElements.define("copied-item", CopiedItemElement);
 
-function toClipboard(data) {
+async function toClipboard(data) {
   if (data instanceof Blob) {
+    const pngBlob = await convert2Png(data);
     window.navigator.clipboard.write([
       new ClipboardItem({
-        [data.type || "text/plain"]: data,
+        [pngBlob.type]: pngBlob,
       }),
     ]);
   } else {
     window.navigator.clipboard.writeText(data);
   }
+}
+
+async function convert2Png(blob) {
+  if (blob.type === "image/png") return blob;
+
+  const $img = await new Promise((resolve) => {
+    const _$img = document.createElement("img");
+    _$img.crossOrigin = "Anonymous";
+    _$img.src = URL.createObjectURL(blob);
+    _$img.addEventListener("load", () => {
+      resolve(_$img);
+    });
+  });
+
+  const $canvas = document.createElement("canvas");
+  $canvas.width = $img.width;
+  $canvas.height = $img.height;
+
+  const ctx = $canvas.getContext("2d");
+  ctx.drawImage($img, 0, 0);
+
+  return await new Promise((resolve) => {
+    $canvas.toBlob((blob) => {
+      resolve(blob);
+    }, "image/png");
+  });
 }
