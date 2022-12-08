b9286984780856b133aa93c63f969c142b75a765
dev2820 <second28200@gmail.com>
2022-12-08 19:28:48 +0900

    feature/ addListener run only once after installed

--- scripts/background.js
+++ scripts/background.js
@@ -5,33 +5,36 @@ const RESPONSE = {
   GET_CONTENT_LIST: "GET_CONTENT_LIST",
   DELETE: "DELETE",
 };
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.action) {
-    case RESPONSE.NEW_CONTENT: {
-      itemRepo.add({ created: new Date(), content: message.payload });
-      return false;
-    }
-    case RESPONSE.GET_CONTENT_LIST: {
-      itemRepo.getAll().then((items) => {
-        sendResponse(items);
-      });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    switch (message.action) {
+      case RESPONSE.NEW_CONTENT: {
+        itemRepo.add({ created: new Date(), content: message.payload });
+        return false;
+      }
+      case RESPONSE.GET_CONTENT_LIST: {
+        itemRepo.getAll().then((items) => {
+          sendResponse(items);
+        });
 
-      return true;
-    }
-    case RESPONSE.DELETE: {
-      itemRepo
-        .remove(message.payload)
-        .then((isSuccess) => sendResponse(isSuccess));
+        return true;
+      }
+      case RESPONSE.DELETE: {
+        itemRepo
+          .remove(message.payload)
+          .then((isSuccess) => sendResponse(isSuccess));
 
-      return true;
+        return true;
+      }
     }
-  }
+  });
+
+  chrome.contextMenus.create({
+    title: "My Context Menu",
+    id: "MyContext",
+  });
 });
 
-chrome.contextMenus.create({
-  title: "My Context Menu",
-  id: "MyContext",
-});
 /**
  * onMessage에서 비동기를 쓰려면 return true를 해줘야한다함
  * https://stackoverflow.com/questions/48107746/chrome-extension-message-not-sending-response-undefined
